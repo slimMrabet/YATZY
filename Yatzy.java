25f1ea90f2c3cb3189e5243607eb6c60f6b9fc47
slimMrabet <slim.mrabet@gmail.com>
2019-07-11 23:31:00 +0200

    [YATZY] Refacto chance method

--- Yatzy.java
+++ Yatzy.java
@@ -34,12 +34,10 @@ public class Yatzy {
     
     public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
+
+        int total = Stream.of(d1, d2, d3, d4, d5)
+        		  .mapToInt(Integer::intValue)
+        		  .sum();
         return total;
     }
 
