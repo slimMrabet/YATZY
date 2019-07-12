15c35f3b03914469f43fb9faae7c84339cd47a97
slimMrabet <slim.mrabet@gmail.com>
2019-07-12 02:00:34 +0200

    [YATZY] Refacto Straight & full house

--- Yatzy.java
+++ Yatzy.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -115,103 +114,88 @@ public class Yatzy {
         	    .forEach(i -> { n[0]++;score[0] += (6-i);});
     	return n[0] == 2 ? score[0] * 2 : 0;
     }
-
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    
+    private static int x_of_a_kind(Map<Integer, Integer> tallies, int padding) {
+    	OptionalInt res  = IntStream
+        	    .range(0, 6)
+        	    .filter(i -> tallies.getOrDefault( i, 0 ) >= padding)
+      			.map(i-> (i+1) * padding )
+      			.findFirst();
+        return res.isPresent() ? res.getAsInt() : 0;
+    }
+    
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    	Map<Integer, Integer> tallies = new HashMap<>(6);
+    	Stream.of(d1, d2, d3, d4, d5).forEach(x -> {
+    		int value = tallies.getOrDefault(x-1, 0) +1;
+    		tallies.put(x -1, value);
+    	}); 
+    	return x_of_a_kind(tallies, 4);
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+    	Map<Integer, Integer> tallies = new HashMap<>(6);
+    	Stream.of(d1, d2, d3, d4, d5).forEach(x -> {
+    		int value = tallies.getOrDefault(x-1, 0) +1;
+    		tallies.put(x -1, value);
+    	}); 
+    	return x_of_a_kind(tallies, 3);
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+    	Map<Integer, Integer> tallies = new HashMap<>(6);
+    	Stream.of(d1, d2, d3, d4, d5).forEach(x -> {
+    		int value = tallies.getOrDefault(x-1, 0) +1;
+    		tallies.put(x -1, value);
+    	});
+    	List<Integer> res = tallies.entrySet().stream()
+    			.filter(count -> count.getKey() >= 0 && count.getKey() < 5 && count.getValue() == 1)
+    			.map(x->x.getValue())
+    			.collect(Collectors.toList());
+    	
+        return res.size() == 5 ? 15 : 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
+    	Map<Integer, Integer> tallies = new HashMap<>(6);
+    	Stream.of(d1, d2, d3, d4, d5).forEach(x -> {
+    		int value = tallies.getOrDefault(x-1, 0) +1;
+    		tallies.put(x -1, value);
+    	});
+    	List<Integer> res = tallies.entrySet().stream()
+    			.filter(count -> count.getKey() >= 1 && count.getKey() < 6 && count.getValue() == 1)
+    			.map(x->x.getValue())
+    			.collect(Collectors.toList());
+    	
+        return res.size() == 5 ? 20 : 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
+    	Map<Integer, Integer> tallies = new HashMap<>(6);
+        boolean[] _2 = {false};
+        int[] _2_at = {0};
+        boolean[] _3 = {false};
+        int[] _3_at = {0};
+        
+    	Stream.of(d1, d2, d3, d4, d5).forEach(x -> {
+    		int value = tallies.getOrDefault(x-1, 0) +1;
+    		tallies.put(x -1, value);
+    	});
+    	
+    	IntStream
+	    .range(0, 6)
+	    .filter(i -> tallies.getOrDefault(i, 0 ) == 2 || tallies.getOrDefault(i, 0 ) == 3)
+	    .forEach(i -> {
+	    	_2[0] = tallies.getOrDefault(i, 0 ) == 2 ? true : _2[0];
+	    	_3[0] = tallies.getOrDefault(i, 0 ) == 3 ? true : _3[0];
+	    	_2_at[0] =  tallies.getOrDefault(i, 0 ) == 2 ?  i+1 : _2_at[0] ;
+	    	_3_at[0] = tallies.getOrDefault(i, 0 ) == 3 ?  i+1 : _3_at[0] ;
+	    });
+        return  (_2[0] && _3[0]) ?  _2_at[0] * 2 + _3_at[0] * 3 : 0;
     }
 }
